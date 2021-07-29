7406ffee3b022bca014626e0c71ec24a216512ed
Bernard Kolobara <me@kolobara.com>
2021-07-29 22:45:58 +0200

    Publish v0.5.0

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -1,9 +1,9 @@
 class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
-  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.3.1/lunatic-macos-amd64.tar.gz"
-  sha256 "de63354e3efd138329e9892b39bd6bbf7a512c08ae652f0347b36ca4a8d50153"
-  version "0.3.1"
+  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.5.0/lunatic-macos-amd64.tar.gz"
+  sha256 "98414e713204b8370331bab20b2e1201c6b68d5be5c9be876d2150db6868dced"
+  version "0.5.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   def install
