77f3e011b36d50c20ffc4aefafc4ab3e8b32a620
Bernard Kolobara <me@kolobara.com>
2021-08-31 20:39:14 +0200

    Bump version to 0.6.0

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -1,9 +1,9 @@
 class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
-  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.5.0/lunatic-macos-amd64.tar.gz"
-  sha256 "98414e713204b8370331bab20b2e1201c6b68d5be5c9be876d2150db6868dced"
-  version "0.5.0"
+  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.6.0/lunatic-macos-amd64.tar.gz"
+  sha256 "f101a6148f2417cf8d01108dcfdd9a4cd5e432aeb82fd0bd3c74c58e766a6b70"
+  version "0.6.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   def install
