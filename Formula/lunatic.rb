e835bc058dd43b33f01ee6b880848dd0feb83af5
Bernard Kolobara <me@kolobara.com>
2021-08-31 21:09:10 +0200

    Fix hash for version 0.6

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -2,7 +2,7 @@ class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
   url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.6.0/lunatic-macos-amd64.tar.gz"
-  sha256 "f101a6148f2417cf8d01108dcfdd9a4cd5e432aeb82fd0bd3c74c58e766a6b70"
+  sha256 "ee817a8312ca7cf71f326c6017f346b574c78d73ffd9eb83c3bedaf9456c7327"
   version "0.6.0"
   license any_of: ["MIT", "Apache-2.0"]
 
