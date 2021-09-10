382963fc25e8b2d2cc8902211fe0be72115d32e1
Bernard Kolobara <me@kolobara.com>
2021-09-10 19:16:57 +0200

    Fix checksum

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -2,7 +2,7 @@ class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
   url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.6.1/lunatic-macos-amd64.tar.gz"
-  sha256 "98414e713204b8370331bab20b2e1201c6b68d5be5c9be876d2150db6868dced"
+  sha256 "1ece5570cd28e75e87bda3c718afaa496ea26f69673bd31870006c43ce274898"
   version "0.6.1"
   license any_of: ["MIT", "Apache-2.0"]
 
