9fa8dd7cefc96d5f374bc385a9a5ce6ad0c8b2a3
Bernard Kolobara <me@kolobara.com>
2022-01-07 20:57:00 +0100

    Update manual macos update

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -2,7 +2,7 @@ class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
   url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.7.0/lunatic-macos-amd64.tar.gz"
-  sha256 "297d9f16d78d671c067c5e9fe85c30b084b7cd6d933d0657710ba0d76f69a999"
+  sha256 "99932331a5dc063cdd69cfb812ca6660f439a5fe2690aa492afdabcaa6442ae9"
   version "0.7.0"
   license any_of: ["MIT", "Apache-2.0"]
 
