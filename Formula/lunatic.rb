ad0d41bafdc75302975a7e15ffd77c28faaad0e3
Bernard Kolobara <me@kolobara.com>
2022-01-20 13:50:18 +0100

    Fix download source

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -1,7 +1,7 @@
 class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
-  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.7.5/lunatic-macos-amd64.tar.gz"
+  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.7.5/lunatic-macos-universal.tar.gz"
   sha256 "9f439d1b5ccc91743c861d56f321737cb42a1b35647db1f3cfd5cae5ace56d79"
   version "0.7.5"
   license any_of: ["MIT", "Apache-2.0"]
