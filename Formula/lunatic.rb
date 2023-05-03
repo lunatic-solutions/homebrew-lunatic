531d49acabb5ab3d0b8dbcc788a05af47243c27e
Github Action 'Update lunatic' workflow <bernard@lunatic.solutions>
2023-05-03 20:21:42 +0000

    Update to 0.13.2

--- Formula/lunatic.rb
+++ Formula/lunatic.rb
@@ -1,9 +1,9 @@
 class Lunatic < Formula
   desc "A universal runtime for fast, robust and scalable server-side applications."
   homepage "https://lunatic.solutions"
-  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.12.0/lunatic-macos-universal.tar.gz"
-  sha256 "cef3c9187596086656d843976f7e84e5f22492f519ebebe70cd9549f4f82653b"
-  version "0.12.0"
+  url "https://github.com/lunatic-solutions/lunatic/releases/download/v0.13.2/lunatic-macos-universal.tar.gz"
+  sha256 "b88299a9ba9044c461810d1f1ce3bf98a49b4a5a2f5261ab0b3857cf62a1d310"
+  version "0.13.2"
   license any_of: ["MIT", "Apache-2.0"]
 
   def install
